5bc2307e35dd3f0b56ac004da93c439e58493032
Johan C <johan44co@gmail.com>
2026-02-13 16:14:38 +0100

    Refactor quote fetching to use Headers constructor for improved header management

--- src/app/(user)/quotes/[id]/page.tsx
+++ src/app/(user)/quotes/[id]/page.tsx
@@ -41,7 +41,7 @@ export default async function QuotePage({ params }: QuotePageProps) {
   let quote;
   try {
     quote = await serverApiClient.getQuote(id, {
-      headers: requestHeaders,
+      headers: new Headers(requestHeaders),
       cache: "no-store",
     });
   } catch {
